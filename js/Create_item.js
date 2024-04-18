28a1d1c3d8786c4da002e01f03ee4a5f40a61465
s1411031013 <grong9913@gmail.com>
2024-04-18 19:54:42 +0800

    0418

---
+++ js/Create_item.js
@@ -0,0 +1,40 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault(); 
+
+    
+    const formData = new FormData(form);
+
+
+    const data = {};
+    formData.forEach((value, key) => {
+        data[key] = value;
+    });
+
+    const url = "http://localhost:5193/api/Back/AddProduct";
+
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data),
+        credentials: 'include'
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('伺服器回應錯誤');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log('新增商品成功:', data);
+        alert('新增商品成功');
+    })
+    .catch(error => {
+        console.error('發生錯誤:', error);
+        alert('新增商品失敗');
+    });
+});
